1f6a5e693cbfc4ecffd1e60b5c789ef483eb0a24
Aidan <aidanthomas64@gmail.com>
2023-09-09 01:07:11 -0700

    added list elements to new GUI layou

--- Windows/mJFrameWindow.java
+++ Windows/mJFrameWindow.java
@@ -20,73 +20,41 @@ public class mJFrameWindow extends javax.swing.JFrame {
                         background_color = new Color(220, 220, 220);
         public static final int BORDER_SIZE = 4, WINDOW_DEFAULT_WIDTH = 1000, WINDOW_DEFAULT_HEIGHT = 500,
                         WINDOW_DEFAULT_X = 300, WINDOW_DEFAULT_Y = 300;
+        private final Logic logic;
 
         /**
          * Creates new form Window
          */
         public mJFrameWindow() {
-
-                setTitle("Sim Game");
-                setSize(800, 500);
-                setMinimumSize(new Dimension(368, 400));
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                getContentPane().setBackground(Color.RED);
-                centerFrame();
+                logic = new Logic();
 
                 initLayout();
+                initComponentsBasedOnLogic(logic.generate_holdings_netWorth_cash());
 
                 setVisible(true);
 
         }
 
+        private void initComponentsBasedOnLogic(double netWorth) {
+                jListHoldings.setListData(logic.getHoldingsForDisplayPurpose());
+                jLabel_cash.setText("USD: $" + new DecimalFormat("0.00").format(logic.getCash()));
+                jLabel_netWorth.setText("NET WORTH: $" + new DecimalFormat("0.00").format(netWorth));
+        }
+
         private void initLayout() {
+
+                setTitle("Sim Game");
+                setSize(800, 500);
+                setMinimumSize(new Dimension(368, 100));
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                getContentPane().setBackground(Color.RED);
+                centerFrame();
 
                 jPanel_left = new JPanel();
                 jPanel_right = new JPanel();
 
-                // Set the background color of the panels (for visualization)
-                jPanel_left.setLayout(new BorderLayout());
-
-                JLabel label1 = new JLabel("Net Worth: $");
-                label1.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-                JLabel label2 = new JLabel("Cash: $");
-                label2.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-
-                // Create a JPanel to hold the labels at the top
-                JPanel labelsPanel = new JPanel();
-                labelsPanel.setBackground(Color.CYAN);
-                labelsPanel.setLayout(new BoxLayout(labelsPanel, BoxLayout.Y_AXIS));
-                labelsPanel.add(label1);
-                labelsPanel.add(label2);
-
-                // Create a scrollable section in the middle
-                JPanel scrollablePanel = new JPanel();
-                scrollablePanel.setLayout(new BoxLayout(scrollablePanel, BoxLayout.Y_AXIS));
-                JScrollPane scrollPane = new JScrollPane(scrollablePanel);
-                scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
-                // Dynamically add items to the scrollable section (you can add components here
-                // as needed)
-                for (int i = 1; i <= 20; i++) {
-                        scrollablePanel.add(new JLabel("Item " + i));
-                }
-
-                // Create two buttons at the bottom
-                JButton button1 = new JButton("Button 1");
-                JButton button2 = new JButton("Button 2");
-
-                // Create a JPanel to hold the buttons at the bottom
-                JPanel buttonsPanel = new JPanel();
-                buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-                buttonsPanel.add(button1);
-                buttonsPanel.add(button2);
-
-                // Add the labels, scrollable section, and buttons to the left panel
-                jPanel_left.add(labelsPanel, BorderLayout.NORTH);
-                jPanel_left.add(scrollPane, BorderLayout.CENTER);
-                jPanel_left.add(buttonsPanel, BorderLayout.SOUTH);
-
-                // ================================================================
+                createLeftPanelLayout();
+                initList();
 
                 jPanel_right.setBackground(Color.GREEN);
                 jPanel_left.setBackground(Color.YELLOW);
@@ -120,7 +88,54 @@ public class mJFrameWindow extends javax.swing.JFrame {
         }
 
         private void createLeftPanelLayout() {
+                // Set the background color of the panels (for visualization)
+                jPanel_left.setLayout(new BorderLayout());
 
+                jLabel_netWorth = new JLabel("Net Worth: $");
+                jLabel_netWorth.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+                jLabel_cash = new JLabel("Cash: $");
+                jLabel_cash.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+
+                // Create a JPanel to hold the labels at the top
+                JPanel labelsPanel = new JPanel();
+                labelsPanel.setBackground(Color.CYAN);
+                labelsPanel.setLayout(new BoxLayout(labelsPanel, BoxLayout.Y_AXIS));
+                labelsPanel.add(jLabel_netWorth);
+                labelsPanel.add(jLabel_cash);
+
+                // Create a scrollable section in the middle
+                JPanel scrollablePanel = new JPanel();
+                scrollablePanel.setLayout(new BoxLayout(scrollablePanel, BoxLayout.Y_AXIS));
+                jScrollpane = new JScrollPane(scrollablePanel);
+                jScrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+                // Dynamically add items to the scrollable section (you can add components here
+                // as needed)
+                for (int i = 1; i <= 20; i++) {
+                        scrollablePanel.add(new JLabel("Item " + i));
+                }
+
+                // Create two buttons at the bottom
+                jButton_refresh = new JButton("refresh");
+                jButton2_help = new JButton("restart");
+
+                // Create a JPanel to hold the buttons at the bottom
+                JPanel buttonsPanel = new JPanel();
+                buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+                buttonsPanel.add(jButton_refresh);
+                buttonsPanel.add(jButton2_help);
+
+                // Add the labels, scrollable section, and buttons to the left panel
+                jPanel_left.add(labelsPanel, BorderLayout.NORTH);
+                jPanel_left.add(jScrollpane, BorderLayout.CENTER);
+                jPanel_left.add(buttonsPanel, BorderLayout.SOUTH);
+
+        }
+
+        private void initList() {
+                mListItem listItem = new mListItem();
+                jListHoldings.setCellRenderer(listItem);
+                jScrollpane.setViewportView(jListHoldings);
         }
 
         private void centerFrame() {
@@ -139,8 +154,10 @@ public class mJFrameWindow extends javax.swing.JFrame {
         private javax.swing.JPanel jPanel_right;
 
         // left
-        private javax.swing.JLabel jLabel_cash;
         private javax.swing.JLabel jLabel_netWorth;
-        private javax.swing.JList<Holding> jList;
+        private javax.swing.JLabel jLabel_cash;
+        private javax.swing.JList<Holding> jListHoldings = new javax.swing.JList<>();;
         private javax.swing.JScrollPane jScrollpane;
+        private javax.swing.JButton jButton_refresh;
+        private javax.swing.JButton jButton2_help;
 }
